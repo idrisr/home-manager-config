441f5201e4f049d8f7eb6ed7f5b854c4524373c6
Idris Raja <idris.raja@gmail.com>
2025-07-15 13:26:18 -0500

    pull out graphical elements

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, lib, graphical ? false, ... }:
 
-{
+lib.optionalAttrs graphical {
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-dark.yaml";
--- profiles/base-packages.nix
+++ profiles/base-packages.nix
@@ -2,25 +2,15 @@ pkgs:
 
 with pkgs; [
   dfc
-  jetbrains-mono
   fd
   file
   htop
   jq
-  lynis
-  monaspace
   ncdu
-  ntfs3g
   pipe-rename
   smem
-  smemstat
   sysz
-  tig
   tmux
   trash-cli
   tree
-  trippy
-  wavemon
-  weechat
-
 ]
