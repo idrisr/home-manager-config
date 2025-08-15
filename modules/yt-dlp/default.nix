af6eb1ddfd27fe3069d1e13508513c661c11a3be
Idris Raja <idris.raja@gmail.com>
2025-08-15 09:38:35 -0500

    update

--- modules/yt-dlp/default.nix
+++ modules/yt-dlp/default.nix
@@ -11,12 +11,12 @@
 
         settings = {
           write-auto-subs = true;
+          write-subs = true;
           embed-chapters = true;
           sub-langs = "en";
           downloader = "aria2c";
           downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
           output = ''%(uploader)s-%(title)s-[%(id)s].%(ext)s'';
-          split-chapters = true;
           restrict-filenames = true;
         };
       };
