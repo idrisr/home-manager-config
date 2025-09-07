816adf5f1fc893abfc3e9ffe0c1e2c6f8c13c82e
Idris Raja <idris.raja@gmail.com>
2025-09-07 18:19:13 -0500

    yt-dlp

--- modules/yt-dlp/default.nix
+++ modules/yt-dlp/default.nix
@@ -6,18 +6,18 @@
         extraConfig = ''
           --paths=home:~/videos/
           --paths=temp:/tmp/
-          --convert-subs srt
         '';
 
         settings = {
-          write-auto-subs = true;
-          write-subs = true;
           embed-chapters = true;
-          sub-langs = "en";
           downloader = "aria2c";
           downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
           output = ''%(uploader)s-%(title)s-[%(id)s].%(ext)s'';
           restrict-filenames = true;
+          sub-langs = "en";
+          write-subs = true;
+          write-auto-subs = true;
+          convert-subs = "srt";
         };
       };
     };
