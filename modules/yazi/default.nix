632f87f016b6aa76563f9584042847331a6d202a
Idris Raja <idris.raja@gmail.com>
2025-09-11 15:01:34 -0500

    formatting

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -40,35 +40,14 @@
       keymap = {
         mgr.prepend_keymap = [
           { run = "quit"; on = [ "q" ]; }
-          {
-            on = [ "g" "v" ];
-            run = "cd ~/videos";
-          }
-          {
-            on = [ "g" "b" ];
-            run = "cd ~/books";
-          }
-          {
-            on = [ "g" "w" ];
-            run = "cd ~/downloads";
-          }
-          {
-            on = [ "g" "t" ];
-            run = "cd ~/documents/tech-talks/";
-          }
-          {
-            on = [ "g" "p" ];
-            run = "cd ~/documents/papers/";
-          }
-          {
-            on = [ "g" "r" ];
-            run = "cd ~/roam-export/";
-          }
-          {
-            on = [ "g" "s" ];
-            run = "cd ~/screenshots/";
-          }
-
+          { on = [ "g" "v" ]; run = "cd ~/videos"; }
+          { on = [ "g" "b" ]; run = "cd ~/books"; }
+          { on = [ "g" "w" ]; run = "cd ~/downloads"; }
+          { on = [ "g" "t" ]; run = "cd ~/documents/tech-talks/"; }
+          { on = [ "g" "p" ]; run = "cd ~/documents/papers/"; }
+          { on = [ "g" "r" ]; run = "cd ~/roam-export/"; }
+          { on = [ "g" "s" ]; run = "cd ~/screenshots/"; }
+          { on = [ "g" "m" ]; run = "cd /var/lib/pinchflat/media"; }
           {
             on = [ "g" "L" ];
             run = ''shell '${pkgs.sorta}/bin/sorta --input "$1" | wl-copy' '';
